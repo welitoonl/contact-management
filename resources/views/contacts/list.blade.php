7dff5db3dc15840445213f6a38d5bf97463323a5
welitoonl <69065140+welitoonl@users.noreply.github.com>
2022-04-23 17:57:27 -0300

    - List style

--- resources/views/contacts/list.blade.php
+++ resources/views/contacts/list.blade.php
@@ -5,7 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><a href="{{ route('contacts.create') }}">Create Contact</a></div>
+                <div class="card-header d-flex justify-content-between">
+                    <h2 class="align-self-center"><i class="fa-solid fa-address-book"></i> Contact list</h2>
+                    <a class="btn btn-lg align-self-center" href="{{ route('contacts.create') }}"><i class="fa-solid fa-circle-plus"></i> Create Contact</a>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,14 +16,14 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    <table>
+                    <table class="table table-striped">
                         <thead>
-                            <th>
-                                <td>Name</td>
-                                <td>Contact</td>
-                                <td>Email</td>
-                                <td>...</td>
-                            </th>
+                            <tr>
+                                <th>Name</th>
+                                <th>Contact</th>
+                                <th>Email</th>
+                                <th class="text-center"></th>
+                            </tr>
                         </thead>
                         <tbody>
                             @foreach($contacts as $contact)
@@ -28,15 +31,15 @@
                                     <td>{{ $contact->name }}</td>
                                     <td>{{ $contact->contact }}</td>
                                     <td>{{ $contact->email }}</td>
-                                    <td>
+                                    <td class="text-center">
                                         <form action="{{ route('contacts.destroy', $contact->id) }}" method="POST">
 
-                                            <a class="btn btn-primary" href="{{ route('contacts.edit', $contact->id) }}">Edit</a>
+                                            <a class="btn btn-warning text-light rounded-start-pill" title="Edit this contact" href="{{ route('contacts.edit', $contact->id) }}"><i class="fa-solid fa-pen-to-square"></i></a>
 
                                             @csrf
                                             @method('DELETE')
 
-                                            <button type="submit" class="btn btn-danger">Delete</button>
+                                            <button type="submit" title="Delete this contact" class="btn btn-danger rounded-end-pill "><i class="fa-solid fa-trash"></i></button>
                                         </form>
                                     </td>
                                 </tr>
